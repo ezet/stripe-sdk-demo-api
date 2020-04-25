65f5046a40b241ce57ea67c3e7c0e25b44a7437b
Lars-Kristian Dahl <lars.dahl@gmail.com>
2020-04-26 01:46:46 +0200

    Support creating setup intent with and without an initial pm

--- functions/src/handlers/createSetupIntentHandler.ts
+++ functions/src/handlers/createSetupIntentHandler.ts
@@ -1,18 +1,21 @@
 import * as functions from "firebase-functions";
 import {getRequestingUserId, getStripeCustomerId, stripe} from "../utils";
+import Stripe = require("stripe");
 
 export async function createSetupIntentHandler(data: any, context: functions.https.CallableContext) {
     const customerId = await getStripeCustomerId(getRequestingUserId(context));
     if (customerId === null) {
         throw new functions.https.HttpsError('failed-precondition', "User has no Stripe ID");
     }
-    const setupIntent = await stripe.setupIntents.create({
-        // confirm: true,
+    const params: Stripe.setupIntents.ISetupIntentCreationOptions = {
         customer: customerId,
-        // payment_method: data.paymentMethod,
         usage: 'on_session',
-        // return_url: data.return_url,
-        // payment_method_types: ["card"],
-    });
+    };
+
+    if (data.paymentMethod) params.payment_method = data.paymentMethod;
+    if (data.returlUrl) params.return_url = data.returnUrl;
+
+
+    const setupIntent = await stripe.setupIntents.create(params);
     return {status: setupIntent.status, clientSecret: setupIntent.client_secret};
 }
